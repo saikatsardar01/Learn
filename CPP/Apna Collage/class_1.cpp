4cf0c095f26ed759e682d09313df4a935cf0ca69
SAIKAT KUMAR SARDAR <153124279+saikatsardar01@users.noreply.github.com>
2024-04-30 11:43:36 +0530

    Update class_1.cpp

--- CPP/Apna Collage/class_1.cpp
+++ CPP/Apna Collage/class_1.cpp
@@ -1,1 +1,14 @@
 Here we only learn about the Algorithm, also some basic logic.
+
+  #include<iostream>
+using namespace std;
+
+int main() 
+{
+ 
+
+return 0;
+
+}
+
+Basic structure of C++ program
