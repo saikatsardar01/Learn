294eba7d6d15686bb36a911bd594aa897a92be8c
SAIKAT KUMAR SARDAR <153124279+saikatsardar01@users.noreply.github.com>
2024-04-30 11:47:37 +0530

    Update class_2.cpp

--- CPP/Apna Collage/class_2.cpp
+++ CPP/Apna Collage/class_2.cpp
@@ -1,1 +1,17 @@
+// Class 2 is about how to install VS Studio and the basic output.
 
+// CODE
+
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+  
+cout << "Hello Guys"<< endl;
+return 0;
+
+}
+
+//  endln; ----> It is used for End line
+// cout ----> For the output
